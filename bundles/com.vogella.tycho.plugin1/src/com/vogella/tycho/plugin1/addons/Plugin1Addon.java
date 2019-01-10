0b7a833214e38474c68d4e60f9b23423c1243bb5
Rabea Gransberger <gransberger@meko-s.local>
2019-01-10 10:49:26 +0100

    add Lombok

--- bundles/com.vogella.tycho.plugin1/src/com/vogella/tycho/plugin1/addons/Plugin1Addon.java
+++ bundles/com.vogella.tycho.plugin1/src/com/vogella/tycho/plugin1/addons/Plugin1Addon.java
@@ -1,4 +1,4 @@
- 
+
 package com.vogella.tycho.plugin1.addons;
 
 import javax.inject.Inject;
@@ -8,15 +8,24 @@ import org.eclipse.e4.core.di.extensions.EventTopic;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.osgi.service.event.Event;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
 @SuppressWarnings("restriction")
 public class Plugin1Addon {
 
+	@Getter
+	@Setter
+	private String test = "value";
+
 	@Inject
 	@Optional
-	public void applicationStarted(
-			@EventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event, @Optional InterfaceOfComplexFrameworkClass complexFrameworkClass) {
-		if(complexFrameworkClass != null) {
-			System.out.println("App startet: " + complexFrameworkClass.getFoo());
+	public void applicationStarted(@EventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event,
+			@Optional InterfaceOfComplexFrameworkClass complexFrameworkClass) {
+		if (complexFrameworkClass != null) {
+			System.out.println("App startet: " + complexFrameworkClass.getFoo() + " " + getTest());
 		}
 	}
 }
